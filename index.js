1eb5f323f288451be280a006b17544850d34f97c
Ray Deck <ray@raydeck.com>
2020-06-13 05:52:17 -0500

    Adding email support and externalizing the initial challenge phase

--- index.js
+++ index.js
@@ -1,27 +1,33 @@
 const { Auth } = require("@aws-amplify/auth");
+const initialChallenge = async (userString, authType) => {
+  let user = await Auth.signIn(userString);
+  if (user.getAuthenticationFlowType() !== "CUSTOM_AUTH") {
+    console.warn(user);
+    throw new Error("Did not get proper authentication flow");
+  }
+  if (user.challengeParam.authType !== "FIRST")
+    throw new Error("Did not get proper authenticatication type response");
+  user = await Auth.sendCustomChallengeAnswer(user, authType, {
+    authType,
+  });
+  if (user.challengeParam.authType !== authType)
+    throw new Error("Did not get proper authentication challenge type");
+  return user;
+};
 const Google = async (token) => {
   const { email } = decode(token);
-  let user = await Auth.signIn(email);
-  user = await Auth.sendCustomChallengeAnswer(user, "GOOGLE", {
-    authType: "GOOGLE",
-  });
+  const user = await initialChallenge(email, "GOOGLE");
   user = await Auth.sendCustomChallengeAnswer(user, jwt);
   return user;
 };
 const Apple = async (token) => {
   const { email } = decode(token);
-  let user = await Auth.signIn(email);
-  user = await Auth.sendCustomChallengeAnswer(user, "APPLE", {
-    authType: "APPLE",
-  });
+  const user = await initialChallenge(email, "APPLE");
   user = await Auth.sendCustomChallengeAnswer(user, jwt);
   return user;
 };
 const Phone = async (phone) => {
-  let user = await Auth.signIn(phone);
-  user = await Auth.sendCustomChallengeAnswer(user, "PHONE", {
-    authType: "PHONE",
-  });
+  let user = await initialChallenge(phone, "PHONE");
   return async (code) => {
     try {
       user = await Auth.sendCustomChallengeAnswer(user, code);
@@ -36,4 +42,20 @@ const Phone = async (phone) => {
     }
   };
 };
-module.exports = { Google, Apple, Phone };
+const Email = async (email) => {
+  let user = await initialChallenge(email, "EMAIL");
+  return async (code) => {
+    try {
+      user = await Auth.sendCustomChallengeAnswer(user, code);
+      if (user.challengeName === "CUSTOM_CHALLENGE") {
+        const e = new Error("Code failed but can try again");
+        e.tryAgain = true;
+        throw e;
+      }
+      return user;
+    } catch (e) {
+      throw e;
+    }
+  };
+};
+module.exports = { Google, Apple, Phone, Email };
